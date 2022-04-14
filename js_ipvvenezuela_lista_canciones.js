1e63e104be9654cdb6897fc6ca2db69f374dafa3
dwlatino <dwlatino.ven@gmail.com>
2022-04-14 15:40:47 -0400

    cambios 14 abr 2022

--- js_ipvvenezuela_lista_canciones.js
+++ js_ipvvenezuela_lista_canciones.js
@@ -1,21 +1,36 @@
-const bandejaCanciones=document.getElementById("bandeja_canciones");
 class Cancion {
-	constructor(nombreCancion, autorCancion, tonalidadRecomendada){
+	constructor(nombreCancion, autorCancion, tonalidadRecomendada, letra){
 		this.nombre=nombreCancion;
 		this.autor=autorCancion;
-		this.tonalidades=tonalidadRecomendada;		
+		this.tonalidades=tonalidadRecomendada;
+		this.letra=letra;	
 	};
 	mostrarInformacion(){
 		return this.nombre +`<br>`+ this.autor +`<br>`;
 	};
+	mostrarLetra(){
+		return this.letra
+	};
 };
-
-var elDiosQueAdoramos=new Cancion("El Dios que adoramos", "Soverein Grace Music", "E-A");
-var graciasCristo=new Cancion("Gracias Cristo", "Soverein Grace Music", "E-A");
+let salmo1v1=new Cancion("Salmo 1 v.1", "Eduardo Osteicoechea", "D-F", "");
+let salmo1v2=new Cancion("Salmo 1 v.2", "Eduardo Osteicoechea", "D-F", `
+<p>
+Como el viento pasan los malvados<br>
+Caerán en juicio condenados<br>
+Su camino de impiedad perecerá<br>
+En el reino del Señor no entrarán
+</p>`);
+let salmo1v3=new Cancion("Salmo 1 v.3", "Eduardo Osteicoechea", "D-F", "");
+let graciasCristo=new Cancion("Gracias Cristo", "Soverein Grace Music", "E-A", "");
 
 
 const cancionesIPV=[
-`${elDiosQueAdoramos.mostrarInformacion()}`,
-`${graciasCristo.mostrarInformacion()}`
+`${salmo1v1.mostrarInformacion()}`,
+`${salmo1v2.mostrarInformacion()}`,
+`${salmo1v3.mostrarInformacion()}`
 ]
+
+const bandejaCanciones=document.getElementById("bandeja_canciones");
 bandejaCanciones.innerHTML=cancionesIPV;
+
+
